e4bb5fe24ff676a4feae505dd97f73e852928879
Julia Loeschel <juloeschel@gmail.com>
2021-10-22 15:14:16 +0200

    get API information

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import Example from './components/Example'
-import { getBySelector } from './lib/dom'
+//import Example from './components/Example'
+//import { getBySelector } from './lib/dom'
 import './styles/index.css'
 
-const example = Example('Vite Starter')
+const url = 'https://pokeapi.co/api/v2/pokemon/'
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => console.log(data.results))
+/*
+function renderCards(pokemons) {
+  const pokeContainer = getBySelector('#pokemon-container')
+  pokemons.forEach(pokemon => {
+    pokeContainer.innerHTML =
+      pokeContainer.innerHTML + `<div>${pokemon.name}</div>`
+  })
+}
+
+renderCards(pokemons)
 
 getBySelector('#app').append(example)
+*/
