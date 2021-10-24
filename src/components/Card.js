76dc155b34ab0247bfc042392ff73cdf6d355197
Julia Loeschel <juloeschel@gmail.com>
2021-10-24 10:50:48 +0200

    change to span

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,9 +10,9 @@ export default function Card({ image, name, weight, moves, height, types }) {
         <h2 class="Card__heading">${name}</h2>
         <img class="Card__image" src="${image}" alt="" />
         <div class= "Card__text">
-        <spam>  height / weight : ${height}cm / ${weight}kg</spam>
-        <spam>  move of pokemon: ${moves}</spam>
-        <spam>  type of pokemon: ${types}</spam>
+        <span>  height / weight : ${height}cm / ${weight}kg</span>
+        <span>  move of pokemon: ${moves}</span>
+        <span>  type of pokemon: ${types}</span>
         </div>
     `,
   })
